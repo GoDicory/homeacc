a9625f241d8e453c938768829f1b046536e60df7
Igor Shilov <Godicory@yandex.ru>
2026-01-27 19:53:59 +0300

    Изучение и добавления Service версия 2

--- src/main/java/com/homeacc/controller/MainController.java
+++ src/main/java/com/homeacc/controller/MainController.java
@@ -1,14 +1,22 @@
 package com.homeacc.controller;
 
+import com.homeacc.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private MainService mainService;
+
+
     @GetMapping
     public String mainTest(Model model){
         model.addAttribute("username", "Джун Программер 27.01.2026");
+        model.addAttribute("serviceTest", mainService.testService());
         return "mainTest";
     }
 }
--- src/main/java/com/homeacc/service/MainService.java
+++ src/main/java/com/homeacc/service/MainService.java
@@ -1,2 +1,13 @@
-package com.homeacc.service;public class MainService {
+package com.homeacc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+    public int testService(){
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        return c;
+    }
 }
